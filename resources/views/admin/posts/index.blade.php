7867ccc8f6693641803e0b2ebc567f1056df4c62
Cung Tran <cung.n.tran@gmail.com>
2017-04-16 00:09:02 -0700

    set up reading posts page

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,6 +2,36 @@
 
 @section('content')
     <h1>Posts</h1>
+
+    <table class="table">
+        <thead>
+          <tr>
+            <th>user</th>
+            <th>category</th>
+            <th>photo</th>
+            <th>title</th>
+            <th>body</th>
+            <th>created</th>
+            <th>updated</th>
+          </tr>
+        </thead>
+        <tbody>
+        @if($posts)
+            @foreach($posts As $posts)
+                <tr>
+                    <td>{{$posts->user_id}}</td>
+                    <td>{{$posts->category_id}}</td>
+                    <td>{{$posts->photo_id}}</td>
+                    <td>{{$posts->title}}</td>
+                    <td>{{$posts->body}}</td>
+                    <td>{{$posts->created_at->diffForHumans()}}</td>
+                    <td>{{$posts->updated_at->diffForHumans()}}</td>
+                </tr>
+            @endforeach
+        @endif
+
+        </tbody>
+    </table>
 @endsection
 
 
